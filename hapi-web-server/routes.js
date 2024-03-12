9c5f08132338b731afd06ef02183f8c9af01729c
hizidha <hi.zidha@gmail.com>
2024-03-12 11:28:58 +0700

    update hapi

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -29,16 +29,44 @@ const routes = [
     },
     {
         // localhost:5000/location?name=John&location=id
-        method: 'GET',
         path: '/location',
+        method: 'GET',
         handler: (request, h) => {
             const { name, location } = request.query;
             return `Hello, ${name} from ${location}`;
         }
     },
     {
+        // Untuk mendapatkan body/payload dari sebuah request
+        path: '/user/login',
+        method: 'POST',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}!`;
+        }
+    },
+    {
+        // Fungsi handler harus selalu mengembalikan sebuah nilai (return())
+        // Parameter h tidak hanya berfungsi untuk menetapkan status kode respons
+        path: '/user/login',
         method: 'GET',
+        handler: (request, h) => {
+            // Terdapat dua cara penulisan untuk lekaukan return
+            // 1
+            // const response = h.response('success');
+            // response.type('text/plain');
+            // response.header('Custom-Header', 'some-value');
+            // return response;   
+
+            // 2
+            return h.response('success')
+                    .type('text/plain')
+                    .header('Custom-Header', 'some-value');
+        }
+    },
+    {
         path: '/hello/{name?}',
+        method: 'GET',
         handler: (request, h) => {
             const { name = "stranger" } = request.params;
             const { lang } = request.query;
